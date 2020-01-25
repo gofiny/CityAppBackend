05f72f48bd5a49e5d775b3077effe375c8ce6bf8
dmitry <gofiny@inbox.ru>
2020-01-25 22:49:34 +0700

    add SQL tables scheme

--- project/db_initial.py
+++ project/db_initial.py
@@ -1,15 +1,16 @@
 '''Инициализация таблиц в базе данных'''
 import asyncio
 import asyncpg
+import config
 
 
 async def connect():
     '''Подключается к БД и возваращает объект соединения'''
     conn = await asyncpg.connect(
-        user="telegram",
-        password="telpass123",
-        database="global_chat",
-        host="s162935.hostiman.com"
+        user=config.USER,
+        password=config.PASSWORD,
+        database=config.DATABASE,
+        host=config.HOST
     )
     return conn
 
@@ -22,53 +23,64 @@ async def create_table(conn, sql):
 
 PLAYERS = '''CREATE TABLE IF NOT EXISTS "players"
 (
-    "id" SERIAL NOT NULL PRIMARY KEY,
+    "uuid" uuid NOT NULL PRIMARY KEY,
     "username" varchar(25) NOT NULL UNIQUE,
-    "vk_id" integer NOT NULL UNIQUE,
-    "token" varchar(64) NOT NULL
+    "user_id" varchar(35) NOT NULL UNIQUE,
+    "token" varchar(64) NOT NULL,
+    "metadata varcahr(30)
 );'''
 
 GAME_OBJECTS = '''CREATE TABLE IF NOT EXISTS "game_objects"
 (
-    "id" SERIAL NOT NULL PRIMARY KEY,
-    "name" varchar(20) NOT NULL,
+    "uuid" uuid NOT NULL PRIMARY KEY,
+    "name" varchar(25) NOT NULL,
     "health" integer NOT NULL,
-    "object_type" varchar(7) NULL
+    "object_type" varchar(15) NULL
 );'''
 
 STATIC_OBJECTS = '''CREATE TABLE IF NOT EXISTS "static_objects"
 (
-    "game_object_ptr_id" integer NOT NULL PRIMARY KEY REFERENCES "game_objects" ("id") ON DELETE cascade
+    "game_object_ptr" uuid NOT NULL PRIMARY KEY REFERENCES "game_objects" ("uuid") ON DELETE cascade
 );'''
 
-DYNAMIC_OBJECTS = '''CREATE TABLE IF NOT EXISTS "dynamic_objects"
+GENERATED_OBJECTS = '''CREATE TABLE IF NOT EXISTS "generated_objects"
 (
-    "game_object_ptr_id" integer NOT NULL PRIMARY KEY REFERENCES "game_objects" ("id") ON DELETE cascade,
-    "power" integer NOT NULL,
-    "speed" integer NOT NULL
+    "game_object_ptr" uuid NOT NULL PRIMARY KEY REFERENCES "game_objects" ("uuiu") ON DELETE cascade
 );'''
+
+PawnObjects = '''CREATE TABLE IF NOT EXIST "pawn_objects"
+(
+    "game_object_ptr" uuid NOT NULL PRIMARY KEY REFERENCES "game_objects" ("uuid") ON DELETE cascade,
+    "max_tasks" integer NOT NULL
+)'''
 
 MAP_OBJECTS = '''CREATE TABLE IF NOT EXISTS "map_objects"
 (
-    "id" SERIAL NOT NULL PRIMARY KEY,
+    "uuid" uuid NOT NULL PRIMARY KEY,
     "x" integer NOT NULL,
     "y" integer NOT NULL,
-    "game_object_id" integer NOT NULL REFERENCES "game_objects" ("id") ON DELETE cascade,
-    "owner_id" integer NULL REFERENCES "players" ("id") ON DELETE cascade
+    "game_object" uuid NOT NULL REFERENCES "game_objects" ("uuid") ON DELETE cascade,
+    "owner" uuid NULL REFERENCES "players" ("uuid") ON DELETE cascade
 );'''
 
-MAP_OBJECTS_GAME_OBJECTS_INDEX = '''CREATE INDEX "map_object_game_object_id_dbce3a33" ON "map_objects" ("game_object_id");'''
-MAP_OBJECTS_OWNER_INDEX = '''CREATE INDEX "map_object_owner_id_c79f0bf2" ON "map_objects" ("owner_id");'''
+PlayerResources = '''CREATE TABLE IF NOT EXIST "players_resources
+(
+    "uuid" uuid NOT NULL PRIMARY KEY,
+    "player" uuid NOT NULL REFERENCES "players" ("uuid") ON DELETE cascade,
+    "money" integer NOT NULL,
+    "wood" integer NOT NULL,
+    "stones" integer NOT NULL
+);'''
 
 
 SQLS = [
     PLAYERS,
     GAME_OBJECTS,
     STATIC_OBJECTS,
-    DYNAMIC_OBJECTS,
+    GENERATED_OBJECTS,
     MAP_OBJECTS,
-    MAP_OBJECTS_GAME_OBJECTS_INDEX,
-    MAP_OBJECTS_OWNER_INDEX
+    PawnObjects,
+    PlayerResources
 ]
 
 
