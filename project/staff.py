0e31635818470f0f72ed15446a6c57714f595cc0
Dmitry <gofiny@inbox.ru>
2020-04-20 20:05:30 +0700

    change float to int

--- project/staff.py
+++ project/staff.py
@@ -798,7 +798,7 @@ async def add_pretask_to_pawn(pool: Pool, object_uuid: str, GP_ID: str, task_nam
         response_dict = {
             "task_uuid": str(task_uuid),
             "target_uuid": str(nearest_obj["mo_uuid"]),
-            "common_time": common_time,
+            "common_time": int(common_time),
             "way": way["way"],
         }
         return response_dict
