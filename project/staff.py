c3f034609bd4b7c173056571a81750ee566197d3
gofiny <gofiny@inbox.ru>
2020-06-15 22:11:46 +0700

    fix

--- project/staff.py
+++ project/staff.py
@@ -1022,5 +1022,5 @@ async def get_pawn_tasks_list(pool: Pool, pawn_uuid: str, GP_ID: str) -> List[Op
             "LEFT JOIN tasks ON tasks.uuid=pt.task "
             "LEFT JOIN players ON mo.owner=players.uuid "
             f"WHERE mo.uuid='{pawn_uuid}' AND players.GP_ID='{GP_ID}' "
-            "ORDER BY start_time DESC"
+            "ORDER BY start_time ASC"
         )
